0843dc5eb01688d892eae276b4cbf4a63a9d9d8d
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-17 02:14:40 +0200

    add Equalizer.Nanophone.Tests

---
+++ test/Equalizer.Nanophone.Tests/RoundRobinAddressRouterShould.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Nanophone.Core;
+using Xunit;
+
+namespace Equalizer.Nanophone.Tests
+{
+    public class RoundRobinAddressRouterShould
+    {
+        [Fact]
+        public void WrapAround()
+        {
+            var oneDotOne = new RegistryInformation("1", 1234, "v2.7");
+            var oneDotTwo = new RegistryInformation("1", 1234, "v2.7");
+            var twoDotOne = new RegistryInformation("2", 1234, "v2.7");
+            var twoDotTwo = new RegistryInformation("2", 1234, "v2.7");
+            var threeDotOne = new RegistryInformation("3", 1234, "v2.7");
+            var threeDotTwo = new RegistryInformation("3", 1234, "v2.7");
+            var instances = new List<RegistryInformation> { oneDotOne, oneDotTwo, twoDotOne, twoDotTwo, threeDotOne, threeDotTwo };
+
+            var router = new RoundRobinAddressRouter();
+
+            // choose first
+            var next = router.Choose(instances);
+            Assert.NotNull(next);
+            Assert.Equal("1", next.Address);
+
+            // choose next
+            next = router.Choose(instances);
+            Assert.NotNull(next);
+            Assert.Equal("2", next.Address);
+
+            // choose next
+            next = router.Choose(instances);
+            Assert.NotNull(next);
+            Assert.Equal("3", next.Address);
+
+            // choose first
+            next = router.Choose(instances);
+            Assert.NotNull(next);
+            Assert.Equal("1", next.Address);
+        }
+    }
+}
