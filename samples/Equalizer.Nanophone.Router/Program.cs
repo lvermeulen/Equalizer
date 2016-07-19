5955b720cf71cb0fe44eed2ff2122a6dcdcdfdd4
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-19 23:35:19 +0200

    add Equalizer.Nanophone.Router sample

---
+++ samples/Equalizer.Nanophone.Router/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Equalizer.Nanophone.Router
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int PORT = 5050;
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls($"http://*:{PORT}")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ samples/Equalizer.Nanophone.Router/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Equalizer.Nanophone.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Nanophone.RegistryHost.ConsulRegistry;
+using NLog.Extensions.Logging;
+
+namespace Equalizer.Nanophone.Router
+{
+    public class Startup
+    {
+        private RegistryClient _registryClient;
+
+        public IConfiguration Configuration { get; }
+
+        private RegistryClient BuildRegistryClient()
+        {
+            var consulConfig = new ConsulRegistryHostConfiguration { IgnoreCriticalServices = true };
+            var consul = new ConsulRegistryHost(consulConfig);
+
+            // XXX investigate config binding from Nancy sample
+            var result = new RegistryClient("urlprefix-", new RoundRobinAddressRouter());
+            result.AddRegistryHost(consul);
+
+            return result;
+        }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .SetBasePath(env.ContentRootPath);
+
+            Configuration = builder.Build();
+
+            _registryClient = BuildRegistryClient();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddNLog();
+
+            app.UseEqualizer(new EqualizerOptions { RegistryClient = _registryClient });
+        }
+    }
+}
