4187f6cb98b74ca95adaf7ed9a0350f2812ffae1
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-19 23:34:15 +0200

    add uri path prefix matching to RegistryClient

--- src/Equalizer.Nanophone/RegistryClient.cs
+++ src/Equalizer.Nanophone/RegistryClient.cs
@@ -9,11 +9,14 @@ namespace Equalizer.Nanophone
 {
     public class RegistryClient
     {
-        private readonly List<IRegistryHost> _registryHosts = new List<IRegistryHost>();
         private readonly IRouter<RegistryInformation> _router;
+        private readonly List<IRegistryHost> _registryHosts = new List<IRegistryHost>();
 
-        public RegistryClient(IRouter<RegistryInformation> router)
+        public string PrefixName { get; }
+
+        public RegistryClient(string prefixName, IRouter<RegistryInformation> router)
         {
+            PrefixName = prefixName;
             _router = router;
         }
 
@@ -27,39 +30,45 @@ namespace Equalizer.Nanophone
             _registryHosts.Remove(registryHost);
         }
 
-        private async Task<IList<RegistryInformation>> FindAllServiceInstancesAsync(string name)
+        public async Task<IList<RegistryInformation>> FindServiceInstancesAsync()
         {
             var allInstances = new List<RegistryInformation>();
 
             foreach (var registryHost in _registryHosts)
             {
-                var instances = await registryHost.FindServiceInstancesAsync(name);
+                var instances = await registryHost.FindServiceInstancesAsync();
                 allInstances.AddRange(instances);
             }
 
             return allInstances;
         }
 
-        private async Task<IList<RegistryInformation>> FindAllServiceInstancesAsync(string name, string version)
+        private bool KeyValuePairMatchesPathAndQuery(KeyValuePair<string, string> keyValuePair, string prefixName, string pathAndQuery)
         {
-            var allInstances = await FindAllServiceInstancesAsync(name);
+            bool prefixEquals = keyValuePair.Key.Equals(prefixName, StringComparison.OrdinalIgnoreCase);
+            bool startsWith = pathAndQuery.StartsWith(keyValuePair.Value, StringComparison.Ordinal);
 
-            return allInstances
-                .Where(x => x.Version.Equals(version, StringComparison.OrdinalIgnoreCase))
+            return prefixEquals && startsWith;
+        }
+
+        public IList<RegistryInformation> FindServiceInstancesAsync(Uri uri, IEnumerable<RegistryInformation> instances)
+        {
+            var pathAndQuery = uri.GetComponents(UriComponents.PathAndQuery, UriFormat.Unescaped);
+            var results = instances
+                .Where(x => x.KeyValuePairs.Any(kvp => KeyValuePairMatchesPathAndQuery(kvp, PrefixName, pathAndQuery)))
                 .ToList();
+
+            return results;
         }
 
-        public async Task<RegistryInformation> Choose(string name)
+        public async Task<IList<RegistryInformation>> FindServiceInstancesAsync(Uri uri)
         {
-            var instances = await FindAllServiceInstancesAsync(name);
-
-            return _router.Choose(instances);
+            var instances = await FindServiceInstancesAsync();
+            return FindServiceInstancesAsync(uri, instances);
         }
 
-        public async Task<RegistryInformation> Choose(string name, string version)
+        public RegistryInformation Choose(IList<RegistryInformation> instances)
         {
-            var instances = await FindAllServiceInstancesAsync(name, version);
-
             return _router.Choose(instances);
         }
     }
