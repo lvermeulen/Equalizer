b817e2b0b7b2e887d64ab4d2b5a4462765e24063
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-20 11:46:44 +0200

    add Equalizer.Nanophone.Middleware.Tests project

---
+++ test/Equalizer.Nanophone.Middleware.Tests/EqualizerNanophoneMiddlewareShould.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Equalizer.Nanophone.Middleware.Tests
+{
+    public class EqualizerNanophoneMiddlewareShould
+    {
+        [Fact]
+        public void Handle()
+        {
+            
+        }
+
+        [Fact]
+        public void NotHandle()
+        {
+            
+        }
+    }
+}
