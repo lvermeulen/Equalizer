1646f2c8cde279e1e72efa0cb3717afe202da5ae
Luk Vermeulen <Luk.Vermeulen@gmail.com>
2016-07-21 13:58:16 +0200

    remove unused usings

--- src/Equalizer.Middleware/ApplicationBuilderExtensions.cs
+++ src/Equalizer.Middleware/ApplicationBuilderExtensions.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Options;
 
--- src/Equalizer.Middleware/EqualizerMiddlewareOptions.cs
+++ src/Equalizer.Middleware/EqualizerMiddlewareOptions.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Equalizer.Middleware.Core;
+﻿using Equalizer.Middleware.Core;
 
 namespace Equalizer.Middleware
 {
