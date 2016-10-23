de934fc9b5765a5e84523d73db15c1cb600846b7
Luk Vermeulen <luk.vermeulen@gmail.com>
2016-10-23 10:43:58 +0200

    add Owin sample

---
+++ samples/Equalizer.LoadBalancer.Owin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Equalizer.LoadBalancer.Owin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string uri = "http://localhost:5051/";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine($"Owin host listening on {uri}, press Enter to stop.");
+                Console.ReadKey();
+                Console.WriteLine("Stopping");
+            }
+        }
+    }
+}
---
+++ samples/Equalizer.LoadBalancer.Owin/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Equalizer.Middleware.Core;
+using Equalizer.Middleware.Owin;
+using Nanophone.RegistryHost.ConsulRegistry;
+using NLog.Owin.Logging;
+using Owin;
+
+namespace Equalizer.LoadBalancer.Owin
+{
+    public class Startup
+    {
+        private RegistryClient BuildRegistryClient(string prefixName)
+        {
+            var consul = new ConsulRegistryHost();
+
+            var result = new RegistryClient(prefixName, new RoundRobinAddressRouter());
+            result.AddRegistryHost(consul);
+
+            return result;
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNLog();
+
+            var registryClient = BuildRegistryClient("urlprefix-");
+            app.UseEqualizer(new EqualizerMiddlewareOptions
+            {
+                RegistryClient = registryClient,
+                PathExclusions = new[] { "/" } // exclude root from redirects
+            });
+        }
+    }
+}
