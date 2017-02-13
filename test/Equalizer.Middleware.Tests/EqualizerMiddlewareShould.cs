210335bcddd37de48f437baf22d3ad3c25bb63a1
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-02-13 20:31:13 +0100

    add ApplicationBuilderExtensions tests

--- test/Equalizer.Middleware.Tests/EqualizerMiddlewareShould.cs
+++ test/Equalizer.Middleware.Tests/EqualizerMiddlewareShould.cs
@@ -123,6 +123,13 @@ namespace Equalizer.Middleware.Tests
             Assert.Throws<ArgumentNullException>(() =>
                 new TestServer(new WebHostBuilder().Configure(app =>
                 {
+                    ApplicationBuilderExtensions.UseEqualizer(null, null);
+                }))
+            );
+
+            Assert.Throws<ArgumentNullException>(() =>
+                new TestServer(new WebHostBuilder().Configure(app =>
+                {
                     app.UseEqualizer(null);
                 }))
             );
