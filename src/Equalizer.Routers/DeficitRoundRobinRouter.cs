3f8a09cd98a9c7c7c482c2a59369262fb5024ff8
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-03-07 21:05:42 +0100

    add DeficitRoundRobinRouter

---
+++ src/Equalizer.Routers/DeficitRoundRobinRouter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Equalizer.Core;
+
+namespace Equalizer.Routers
+{
+    public class DeficitRoundRobinRouter<T> : IRouter<T>
+        where T : class
+    {
+        private readonly IDictionary<T, int> _quanta;
+        private readonly Dictionary<T, int> _counters = new Dictionary<T, int>();
+
+        public DeficitRoundRobinRouter(IDictionary<T, int> quanta)
+        {
+            _quanta = quanta;
+        }
+
+        public T Choose(IList<T> instances)
+        {
+            foreach (var instance in instances)
+            {
+                int quantum;
+                if (_quanta.TryGetValue(instance, out quantum))
+                {
+                    int counter;
+                    if (!_counters.TryGetValue(instance, out counter))
+                    {
+                        counter = 0;
+                    }
+
+                    if (counter < quantum)
+                    {
+                        _counters[instance] = counter + 1;
+
+                        // reset all counters if we're at the end
+                        if (instance == instances.LastOrDefault() && counter == quantum - 1)
+                        {
+                            _counters.Clear();
+                        }
+
+                        return instance;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ test/Equalizer.Routers.Tests/DeficitRoundRobinRouterShould.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Equalizer.Routers.Tests
+{
+    public class DeficitRoundRobinRouterShould
+    {
+        [Fact]
+        public void WrapAround()
+        {
+            string first = "1";
+            string second = "2";
+            string third = "3";
+            var instances = new List<string> { first, second, third };
+            var quanta = new Dictionary<string, int>
+            {
+                [first] = 1,
+                [second] = 2,
+                [third] = 3
+            };
+
+            var router = new DeficitRoundRobinRouter<string>(quanta);
+
+            // choose first X 1
+            string next = router.Choose(instances);
+            Assert.Equal(first, next);
+
+            // choose second X 2
+            next = router.Choose(instances);
+            Assert.Equal(second, next);
+            next = router.Choose(instances);
+            Assert.Equal(second, next);
+
+            // choose third X 3
+            next = router.Choose(instances);
+            Assert.Equal(third, next);
+            next = router.Choose(instances);
+            Assert.Equal(third, next);
+            next = router.Choose(instances);
+            Assert.Equal(third, next);
+
+            // choose first X 1
+            next = router.Choose(instances);
+            Assert.Equal(first, next);
+
+            // choose second X 2
+            next = router.Choose(instances);
+            Assert.Equal(second, next);
+            next = router.Choose(instances);
+            Assert.Equal(second, next);
+
+            // choose third X 3
+            next = router.Choose(instances);
+            Assert.Equal(third, next);
+            next = router.Choose(instances);
+            Assert.Equal(third, next);
+            next = router.Choose(instances);
+            Assert.Equal(third, next);
+        }
+
+        [Fact]
+        public void HaveQuanta()
+        {
+            var instances = new List<string> { "1" };
+            var router = new DeficitRoundRobinRouter<string>(new Dictionary<string, int>());
+
+            // choose first X 1
+            //string next = ;
+            Assert.Null(router.Choose(instances));
+        }
+    }
+}
